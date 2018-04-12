974a9f13935123af5b5a6a1f9704c206dcb3975a
Ignacio <ilopez-amor@ya.com>
2018-04-12 21:11:43 +0200

    adding tests

---
+++ src/AppNavigator.unit.test.js
@@ -0,0 +1,15 @@
+import {s hallow } from '@vue/test-vue'
+import AppNavigator from './AppNavigator'
+
+describe('AppNavigator', () => {
+
+  let wrapper
+
+  beforeEach(() => {})
+
+  it('Debe comprobar si el usuario está autenticado')
+
+  it('carga la página HomePage si el usuario SI está autenticado')
+
+  it('carga la página SignUp si el usuario NO está autenticado')
+})
