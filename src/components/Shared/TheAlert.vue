2122fb10ec313dcd268bef8e619f639c70f163da
Ignacio <buscemi@ya.com>
2018-07-27 13:49:35 +0200

    updated alert component

--- src/components/Shared/TheAlert.vue
+++ src/components/Shared/TheAlert.vue
@@ -1,39 +1,16 @@
-<template>
-	<div class="alert">
-		<v-ons-alert-dialog
-			class="AlertDialog_text" 
-			modifier="rowfooter"
-			:visible.sync="isVisible"
-		>
-			{{ alertText }}
-		</v-ons-alert-dialog>
-		<!-- template slot="footer">
-			<v-ons-alert-dialog-button
-				class="alertDialog_button"
-				ripple="true"
-				@click.prevent="onClick()"
-			>
-				{{ alertButtonText}}
-			</v-ons-alert-dialog-button>
-		</template-->
-	</div>
+<template id="alert">
+	<v-ons-card>
+		<div class="title">
+		</div>
+		<div class="content">
+		</div>
+	</v-ons-card>
 </template>
 
 <script>
 	export default {
 		name: 'the-alert',
 		props: {
-			alertText: {
-      type: String,
-      default: ''
-    	},
-    	alertButtonText: {
-    		type: String,
-    		default: 'OK'
-    	},
-    	isVisible: {
-    		type: Boolean,
-    		default: false
     	}
 		},
 		methods: {
