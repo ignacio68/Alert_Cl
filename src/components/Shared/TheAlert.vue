85c36d9c35df05a775812b26b0b433c8bf6f7a0f
Ignacio <ilopez-amor@ya.com>
2018-09-26 23:27:05 +0200

    adding countdown

--- src/components/Shared/TheAlert.vue
+++ src/components/Shared/TheAlert.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-ons-card class="alertCard" modifier="material">
+  <v-ons-card class="alertCard">
     <div class="title">
     
     </div>
@@ -7,18 +7,19 @@
     <div class="content">
 
       <v-ons-row>
-        <v-ons-col width="15%">
+          <h4 class="alertCard__countDown">{{ days }}:{{ hours }}:{{ minutes }}:{{ seconds }}</h4>
+      </v-ons-row>
+
+      <v-ons-row>
+        <v-ons-col width="21%">
           <img
             :src="userIcon"
             :alt="altIcon"
             class="alertCard__userIcon"
           >
         </v-ons-col>
-        <v-ons-col width="65%">
-          <h3 class="alertCard__userName">{{ userName }}</h3>
-        </v-ons-col>
         <v-ons-col>
-          <h4 class="alertCard__countDown">{{ countDown }}</h4>
+          <h3 class="alertCard__userName">{{ userName }}</h3>
         </v-ons-col>
       </v-ons-row>
 
@@ -31,23 +32,28 @@
       </v-ons-row>
         
       <v-ons-row>
-        <v-ons-button 
-          class="alertCard__button"
-          ripple="true"
-          @click.prevent="onPhoneClick()"
-        >
-          {{ phoneButton }}
-        </v-ons-button>
-        <v-ons-button 
-          class="alertCard__button"
-          ripple="true"
-          @click.prevent="onLinkClick()"
-        >
-          {{ linkButton }}
-        </v-ons-button>
-        <v-ons-button ripple="true">
-          <v-ons-icon icon="ion-share, material:md-share"/>
-        </v-ons-button>
+        <div class="alertCard-buttons">
+          <v-ons-button 
+            class="alertCard__button"
+            ripple="true"
+            @click.prevent="onPhoneClick()"
+          >
+            {{ phoneButton }}
+          </v-ons-button>
+          <v-ons-button 
+            class="alertCard__button"
+            ripple="true"
+            @click.prevent="onLinkClick()"
+          >
+            {{ linkButton }}
+          </v-ons-button>
+          <v-ons-button
+            class="alertCard__button" 
+            ripple="true"
+          >
+            <v-ons-icon icon="ion-share, material:md-share"/>
+          </v-ons-button>
+        </div>
       </v-ons-row>
       
     </div>
@@ -70,7 +76,7 @@
         type: String,
         default: ''
       },
-      countDown: {
+      endDate: {
         type: Number,
         default: ''
       },
@@ -91,33 +97,73 @@
         default: ''
       }
     },
+    data () {
+      return {
+        startDate: '',
+        interval: '',
+        days: '',
+        hours: '',
+        minutes: '',
+        seconds: ''
+      }
+    },
+    mounted () {
+      this.startDate = new Date()
+      // Actualizamos cada segundo
+      this.timerCount(this.startDate, this.endDate)
+      this.interval = setInterval(() => {
+        this.timerCount(this.startDate, this.endDate)
+      }, 1000)
+    },
     methods: {
       onPhoneClick () {
         this.$emit('phonetButtonEvent')
       },
       onLinkClick () {
         this.$emit('linkButtonEvent')
+      },
+      timerCount (startDate, endDate) {
+        let timeRemaining = Math.floor((this.endDate - this.starDate) / 1000)
+
+        if (timeRemaining >= 0) {
+          this.days = Math.floor(timeRemaining / 86400)
+          this.hours = Math.floor(timeRemaining / 3600)
+          this.minutes = Math.floor(timeRemaining / 60)
+          this.seconds = Math.floor(timeRemaining)
+        }
       }
     }
   }
 </script>
 
 <style scoped>
-.alertCard__userIcon {
-  height: 50px;
-  width: 50px;
-}
-.alertCard__userName {
-  padding-left: 5px;
-  text-align: left;
-  font-weight: bold;
-  border: 1px solid red;
-  color: black;
-}
-.alertCard__countDown {
-  text-align: right;
-  font-size: 1em;
-  color: red;
-  border: 1px solid red;
-}
+  .alertCard {
+    border: 1px solid red;
+  }
+  .alertCard__countDown {
+    text-align: right;
+    font-size: 1em;
+    color: red;
+    border: 1px solid blue;
+  }
+  .alertCard__userIcon {
+    height: 50px;
+    width: 50px;
+    border: 1px solid blue;
+  }
+  .alertCard__userName {
+    padding-left: 5px;
+    text-align: left;
+    font-weight: bold;
+    color: black;
+    border: 1px solid green;
+  }
+  .alertCard-buttons {
+    float: right;
+    right: 0px;
+    border: 1px solid green;
+  }
+  .alertCard__button{
+    margin-left: 5px;
+  }
 </style>
--- src/pages/Main/Alerts_old.vue
+++ src/pages/Main/Alerts_old.vue
@@ -1,17 +1,16 @@
 <template>
-  <v-ons-page>
     <div class="container">
       <h1>{{ $t('lang.pages.alerts.main.text')}}</h1>
       <v-ons-list class="alertsList">
         <v-ons-list-item
           :modifier="md ? 'nodivider' : ''"
           class="alertsList__item"
-          v-for="(alert, index) in alerts" :key="alert.userName">
+          v-for="(alert, index) in alerts" :key="alert.userName" tappable>
           <the-alert
             :userIcon="alert.userIcon"
             :altIcon="alert.altIcon"
             :userName="alert.userName"
-            :endDate="alert.endDate"
+            :countDown="alert.countDown"
             :alertTitle="alert.alertTitle"
             :alertText="alert.alertText"
             :phoneButton="$t('lang.components.alerts.phoneButton')"
@@ -27,7 +26,7 @@
         ripple="true"
         @click.prevent="isAlertVisible = true"
       >
-        <v-ons-icon
+        <v-ons-icon 
           class="alertScript__icon"
           icon="ion-edit, material:zmdi-email-open"
         >
@@ -40,12 +39,11 @@
         <span slot="title">ALERTA</span>
           <p>Aquí va la alerta</p>
         <template slot="footer">
-          <v-ons-alert-dialog-button @click.prevent="isAlertVisible = false">Cancel</v-ons-alert-dialog-button>
           <v-ons-alert-dialog-button @click.prevent="isAlertVisible = false">Ok</v-ons-alert-dialog-button>
+          <v-ons-alert-dialog-button @click.prevent="isAlertVisible = false">Cancel</v-ons-alert-dialog-button>
         </template>
       </v-ons-alert-dialog>
     </div>
-  </v-ons-page>
 </template>
 
 <script>
@@ -62,8 +60,8 @@
             userIcon: 'src/assets/Real-Madrid-logo-256.png',
             altIcon: 'Real Madrid logo',
             userName: 'Real Madrid',
-            endDate: 2563698,
-            alertTitle: 'Asientos partido Real Madrid-Barcelona',
+            countDown: 2563698,
+            alertTitle: 'Asientos partido Real Madrid-Atlético',
             alertText: 'Todavía quedan algunos asientos libres para el Clásico de esta tarde',
             alertLink: 'https://www.realmadrid.com/entradas'
           },
@@ -71,7 +69,7 @@
             userIcon: 'src/assets/Atletico-Madrid-logo-256.png',
             altIcon: 'Atletico de Madrid logo',
             userName: 'Atlético de Madrid',
-            endDate: 4589752,
+            countDown: 4589752,
             alertTitle: 'Palco VIP partido Atlético-Celta',
             alertText: 'Tenemos un palco Vip en el primer anfiteatro para el partido de esta noche. 1.000€',
             alertLink: 'https://www.atleticodemadrid.com/entradas'
@@ -90,10 +88,10 @@
 </script>
 
 <style scoped>
-  .alertScript {
-    background-color: dodgerblue;
-  }
-  .alertScript__icon {
-    color: white;
-  }
+.alertScript {
+  background-color: dodgerblue;
+}
+.alertScript__icon {
+  color: white;
+}
 </style>
